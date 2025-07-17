00c6004cd998ac5c55041e283b3d8855002d1db0
Pedro Solano <solanvp@gmail.com>
2025-07-16 20:45:53 -0600

    string only signal

--- plugin/src/main/java/org/godotengine/plugin/appodeal/android/GodotAndroidPlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/appodeal/android/GodotAndroidPlugin.kt
@@ -3,6 +3,8 @@ package org.godotengine.plugin.appodeal.android
 import android.util.Log
 import android.widget.Toast
 import com.appodeal.ads.Appodeal
+import com.appodeal.ads.initializing.ApdInitializationCallback
+import com.appodeal.ads.initializing.ApdInitializationError
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.SignalInfo
@@ -15,19 +17,67 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
     override fun getPluginSignals(): Set<SignalInfo> {
         val signals = mutableSetOf<SignalInfo>()
         signals.add(SignalInfo("signal_test", String::class.java))
+        signals.add(SignalInfo("appodeal_initialized", String::class.java))
+        signals.add(SignalInfo("banner_shown", String::class.java))
         return signals
     }
 
-    /**
-     * Example showing how to declare a method that's used by Godot.
-     *
-     * Shows a 'Hello World' toast.
-     */
     @UsedByGodot
-    fun helloWorld() {
+    fun initialize_appodeal(appKey: String) {
+        activity?.let { safeActivity ->
+            Log.d(pluginName, "Initializing Appodeal with key: $appKey")
+            
+            Appodeal.initialize(
+                context = safeActivity,
+                appKey = appKey,
+                adTypes = Appodeal.BANNER or Appodeal.INTERSTITIAL or Appodeal.REWARDED_VIDEO,
+                callback = object : ApdInitializationCallback {
+                    override fun onInitializationFinished(errors: List<ApdInitializationError>?) {
+                        runOnUiThread {
+                            if (errors == null) {
+                                Log.v(pluginName, "Appodeal initialized successfully")
+                                emitSignal("appodeal_initialized", "true")
+                            } else {
+                                Log.e(pluginName, "Appodeal initialization failed: $errors")
+                                emitSignal("appodeal_initialized", "false")
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+
+    @UsedByGodot
+    fun show_banner() {
         runOnUiThread {
-            Toast.makeText(activity, "Hello from Appodeal", Toast.LENGTH_LONG).show()
-            Log.v(pluginName, "Hello from Appodeal")
+            activity?.let { safeActivity ->
+                Log.d(pluginName, "Attempting to show banner ad")
+                val result = Appodeal.show(safeActivity, Appodeal.BANNER_BOTTOM)
+                Log.d(pluginName, "Banner show result: $result")
+                emitSignal("banner_shown", "true")
+            }
+        }
+    }
+
+    @UsedByGodot
+    fun hide_banner() {
+        runOnUiThread {
+            activity?.let { safeActivity ->
+                Log.d(pluginName, "Hiding banner ad")
+                Appodeal.hide(safeActivity, Appodeal.BANNER_BOTTOM)
+            }
+        }
+    }
+
+    @UsedByGodot
+    fun show_interstitial() {
+        runOnUiThread {
+            activity?.let { safeActivity ->
+                Log.d(pluginName, "Attempting to show interstitial ad")
+                val result = Appodeal.show(safeActivity, Appodeal.INTERSTITIAL)
+                Log.d(pluginName, "Interstitial show result: $result")
+            }
         }
     }
 
@@ -39,5 +89,15 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
             Log.v(pluginName, "Appodeal version $version")
             emitSignal("signal_test", "Hello from Appodealplugin")
         }
+    }
+
+    @UsedByGodot
+    fun is_banner_loaded(): Boolean {
+        return Appodeal.isLoaded(Appodeal.BANNER)
+    }
+
+    @UsedByGodot
+    fun is_interstitial_loaded(): Boolean {
+        return Appodeal.isLoaded(Appodeal.INTERSTITIAL)
     }
 }
