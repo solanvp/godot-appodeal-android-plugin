a0b697a9b2120f80ec407fd5b657127184d12b33
Pedro Solano <solanvp@gmail.com>
2025-07-20 12:51:04 -0600

    removed toast

--- plugin/src/main/java/org/godotengine/plugin/appodeal/android/GodotAndroidPlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/appodeal/android/GodotAndroidPlugin.kt
@@ -1,7 +1,6 @@
 package org.godotengine.plugin.appodeal.android
 
 import android.util.Log
-import android.widget.Toast
 import com.appodeal.ads.Appodeal
 import com.appodeal.ads.initializing.ApdInitializationCallback
 import com.appodeal.ads.initializing.ApdInitializationError
@@ -19,16 +18,24 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
         signals.add(SignalInfo("signal_test", String::class.java))
         signals.add(SignalInfo("appodeal_initialized", String::class.java))
         signals.add(SignalInfo("banner_shown", String::class.java))
-        signals.add(SignalInfo("event_logged", String::class.java))
+        signals.add(SignalInfo("rewarded_loaded", String::class.java))
+        signals.add(SignalInfo("rewarded_failed", String::class.java))
+        signals.add(SignalInfo("rewarded_shown", String::class.java))
+        signals.add(SignalInfo("rewarded_show_failed", String::class.java))
+        signals.add(SignalInfo("rewarded_clicked", String::class.java))
+        signals.add(SignalInfo("rewarded_finished", String::class.java))
+        signals.add(SignalInfo("rewarded_closed", String::class.java))
+        signals.add(SignalInfo("rewarded_expired", String::class.java))
+        signals.add(SignalInfo("interstitial_shown", String::class.java))
+        signals.add(SignalInfo("interstitial_failed", String::class.java))
         return signals
     }
-
+    
 
     @UsedByGodot
     fun check_appodeal() {
         runOnUiThread {
             val version = Appodeal.getVersion()
-            Toast.makeText(activity, "Appodeal version $version", Toast.LENGTH_LONG).show()
             Log.v(pluginName, "Appodeal version $version")
             emitSignal("signal_test", "Hello from Appodealplugin")
         }
@@ -57,11 +64,71 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
                     }
                 }
             )
+            Appodeal.setRewardedVideoCallbacks(object : com.appodeal.ads.RewardedVideoCallbacks {
+                override fun onRewardedVideoLoaded(isPrecache: Boolean) {
+                    Log.d(pluginName, "Rewarded video loaded, isPrecache: $isPrecache")
+                    emitSignal("rewarded_loaded", isPrecache.toString())
+                }
+                override fun onRewardedVideoFailedToLoad() {
+                    Log.e(pluginName, "Rewarded video failed to load!")
+                    emitSignal("rewarded_failed", "false")
+                }
+                override fun onRewardedVideoShown() {
+                    Log.d(pluginName, "Rewarded video shown!")
+                    emitSignal("rewarded_shown", "true")
+                }
+                override fun onRewardedVideoShowFailed() {
+                    Log.e(pluginName, "Rewarded video show failed!")
+                    emitSignal("rewarded_show_failed", "false")
+                }
+                override fun onRewardedVideoClicked() {
+                    Log.d(pluginName, "Rewarded video clicked!")
+                    emitSignal("rewarded_clicked", "true")
+                }
+                override fun onRewardedVideoFinished(amount: Double, currency: String) {
+                    Log.d(pluginName, "Rewarded video finished! Amount: $amount, Currency: $currency")
+                    emitSignal("rewarded_finished", "true")
+                }
+                override fun onRewardedVideoClosed(finished: Boolean) {
+                    Log.d(pluginName, "Rewarded video closed! Finished: $finished")
+                    emitSignal("rewarded_closed", finished.toString())
+                }
+                override fun onRewardedVideoExpired() {
+                    Log.d(pluginName, "Rewarded video expired!")
+                    emitSignal("rewarded_expired", "false")
+                }
+            })
+            Appodeal.setInterstitialCallbacks(object : com.appodeal.ads.InterstitialCallbacks {
+                override fun onInterstitialLoaded(isPrecache: Boolean) {
+                    Log.d(pluginName, "Interstitial loaded, isPrecache: $isPrecache")
+                }
+                override fun onInterstitialFailedToLoad() {
+                    Log.e(pluginName, "Interstitial failed to load!")
+                    emitSignal("interstitial_failed", "false")
+                }
+                override fun onInterstitialShown() {
+                    Log.d(pluginName, "Interstitial shown!")
+                    emitSignal("interstitial_shown", "true")
+                }
+                override fun onInterstitialShowFailed() {
+                    Log.e(pluginName, "Interstitial show failed!")
+                    emitSignal("interstitial_failed", "false")
+                }
+                override fun onInterstitialClicked() {
+                    Log.d(pluginName, "Interstitial clicked!")
+                }
+                override fun onInterstitialClosed() {
+                    Log.d(pluginName, "Interstitial closed!")
+                }
+                override fun onInterstitialExpired() {
+                    Log.d(pluginName, "Interstitial expired!")
+                }
+            })
         }
     }
 
     @UsedByGodot
-    fun is_banner_loaded(): Boolean {
+    fun is_banner_ready(): Boolean {
         return Appodeal.isLoaded(Appodeal.BANNER)
     }
 
@@ -99,21 +166,30 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
     }
 
     @UsedByGodot
-    fun is_interstitial_loaded(): Boolean {
+    fun show_rewarded() {
+        runOnUiThread {
+            activity?.let { safeActivity ->
+                Log.d(pluginName, "Attempting to show rewarded ad")
+                val result = Appodeal.show(safeActivity, Appodeal.REWARDED_VIDEO)
+                Log.d(pluginName, "Rewarded show result: $result")
+            }
+        }
+    }
+
+    @UsedByGodot
+    fun is_interstitial_ready(): Boolean {
         return Appodeal.isLoaded(Appodeal.INTERSTITIAL)
     }
 
     @UsedByGodot
     fun log_event(eventName: String) {
         Log.d(pluginName, "Logging event: $eventName")
-        // Appodeal.trackEvent(eventName, null)
-        emitSignal("event_logged", eventName)
+        Appodeal.logEvent(eventName, null)
     }
 
     @UsedByGodot
     fun log_event_with_parameters(eventName: String, parameters: Map<String, Any?>) {
         Log.d(pluginName, "Logging event: $eventName with parameters: $parameters")
-        // Appodeal.trackEvent(eventName, parameters)
-        emitSignal("event_logged", eventName)
+        Appodeal.logEvent(eventName, parameters)
     }
 }
